3d8350391129867ddd83f38f316d3cd807ebe67b
Matt Levy <milevy1@gmail.com>
2019-07-29 14:59:24 -0600

    Adds comment about configuring debug_errors for endpoints

--- config/dev.exs
+++ config/dev.exs
@@ -15,7 +15,7 @@ config :koroibos, Koroibos.Repo,
 # with webpack to recompile .js and .css sources.
 config :koroibos, KoroibosWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: true,     # change this to false to remove errors from endpoints
   code_reloader: true,
   check_origin: false,
   watchers: []
