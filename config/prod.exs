cbd2b853f06fa8fa8ce5b0c5cd63f45cc640b02f
Matt Levy <milevy1@gmail.com>
2019-07-30 20:22:35 -0600

    Changes config/prod.exs to use Heroku host

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :koroibos, KoroibosWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "koroibos-elixir", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
